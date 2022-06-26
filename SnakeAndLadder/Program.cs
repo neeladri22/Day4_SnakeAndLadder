f3ec814abc8ba85bdd78188f5a7d291f86d1141e
unknown <neeladri22@gmail.com>
2022-06-26 17:27:36 +0530

    Added UC2 program

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,10 +11,21 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Program.UC1();
+            Program.UC2();
         }
+
+        // UC1 Program file
         public static void UC1()
         {
             Console.WriteLine("Snake and ladder game played with singel player start at zero");
+        }
+
+        // UC2 Program file
+        public static void UC2()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 7);
+            Console.WriteLine("The Random player dice number is: " + number);
         }
     }
 }
