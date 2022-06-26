cd4975176ad162da363092da265b0390c419d332
unknown <neeladri22@gmail.com>
2022-06-26 17:13:22 +0530

    first commit

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.UC1();
+        }
+        public static void UC1()
+        {
+            Console.WriteLine("Snake and ladder game played with singel player start at zero");
+        }
+       
+       
+    }
+}
